ca58fab79c9fc5d37d69f643f74b4d891d5d6b81
potatoes <cd64@mail.com>
2022-05-26 18:06:33 -0400

    Start treeview work

--- toodue-desktop-app/ViewModels/MainWindowViewModel.cs
+++ toodue-desktop-app/ViewModels/MainWindowViewModel.cs
@@ -7,5 +7,6 @@ namespace toodue_desktop_app.ViewModels
 	public class MainWindowViewModel : ViewModelBase
 	{
 		public string Greeting => "Toodue!";
+		public string[] Data => new string[] { "- Do Work", "- Gamer Time", "- Troll Actively" };
 	}
 }
